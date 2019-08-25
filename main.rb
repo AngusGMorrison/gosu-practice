df5f93bee9d4fad9daeb6220078212de0d5570c9
github username <contact@angus-morrison.com>
2019-08-25 16:08:09 +0100

    Adds stars and star collection

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require './zorder'
 
 class Tutorial < Gosu::Window
     def initialize()
@@ -6,8 +7,13 @@ class Tutorial < Gosu::Window
         self.caption = "Tutorial Game"
 
         @background_image = Gosu::Image.new("media/space.png", tileable: true)
+
         @player = Player.new
         @player.warp(320, 240)
+
+        #Loads and divides an image into an array of equally sized tiles
+        @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
+        @stars = Array.new
     end
 
     #Called 60 times per second by default
@@ -23,6 +29,8 @@ class Tutorial < Gosu::Window
             @player.accelerate
         end
         @player.move
+        @player.collect_stars(@stars)
+        generate_stars
     end
 
     #Called 60 times per second by default. May be skipped for performance reasons
@@ -31,6 +39,7 @@ class Tutorial < Gosu::Window
         @player.draw
         #Image is drawn at (0, 0). Third argument is z-position.
         @background_image.draw(0, 0, 0)
+        @stars.each { | star | star.draw }
     end
 
     def button_down(id)
@@ -39,6 +48,12 @@ class Tutorial < Gosu::Window
         else
             #The Window member function button_down(id) contains default keybindings for window control that we want to keep, hence super mops up any bindings not explicitly covered by button_down.
             super
+        end
+    end
+
+    def generate_stars
+        if rand(100) < 4 && @stars.size < 25
+            @stars << Star.new(@star_anim)
         end
     end
 end
@@ -85,6 +100,35 @@ class Player
         @image.draw_rot(@x, @y, 1, @angle)
     end
 
+    def score
+        @score
+    end
+
+    def collect_stars(stars)
+        stars.reject! { | star | Gosu.distance(@x, @y, star.x, star.y) < 35 }
+    end
+
+end
+
+
+class Star
+    attr_reader :x, :y
+
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256 - 40) + 40
+        @color.green = rand(256 - 40) + 40
+        @color.blue = rand(256 - 40) + 40
+        @x = rand * 640
+        @y = rand * 480
+    end
+
+    def draw
+        #Every 100 milliseconds, the next index of the animiation array is accessed
+        img = @animation[Gosu.milliseconds / 100 % @animation.size]
+        img.draw(@x - (img.width / 2.0), @y - (img.height / 2.0), ZOrder::STARS, 1, 1, @color, :add)
+    end
 end
 
 #Show does not return until the window is closed by the user or close() is called
