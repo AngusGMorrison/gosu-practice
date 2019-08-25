36d91660f8c3e3e73901e7b78eb9d118818ef8b0
github username <contact@angus-morrison.com>
2019-08-25 18:57:34 +0100

    Completes Settings refactor

--- main.rb
+++ main.rb
@@ -7,15 +7,15 @@ class Tutorial < Gosu::Window
         super(Settings::WINDOW_WIDTH, Settings::WINDOW_HEIGHT)
         self.caption = Settings::CAPTION
 
-        @background_image = Settings::BACKGROUND
+        @background_image = Gosu::Image.new("media/space.png", tileable: true)
 
         @player = Player.new(Settings::NEW_PLAYER_X, Settings::NEW_PLAYER_Y)
 
         #Loads and divides an image into an array of equally sized tiles
         @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
-        @stars = Array.new
+        @stars = Settings::STARTING_STARS
 
-        @font = Gosu::Font.new(20)
+        @font = Settings::SCORE_FONT
     end
 
     #Called 60 times per second by default
@@ -40,9 +40,9 @@ class Tutorial < Gosu::Window
     def draw
         @player.draw
         #Image is drawn at (0, 0). Third argument is z-position.
-        @background_image.draw(0, 0, 0)
+        @background_image.draw(0, 0, ZOrder::BACKGROUND)
         @stars.each { | star | star.draw }
-        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+        @font.draw("Score: #{@player.score}", Settings::SCORE_X, Settings::SCORE_Y, ZOrder::UI, 1.0, 1.0, Settings::SCORE_COLOR)
     end
 
     def button_down(id)
@@ -55,7 +55,7 @@ class Tutorial < Gosu::Window
     end
 
     def generate_stars
-        if rand(100) < 4 && @stars.size < 25
+        if rand(100) < Settings::STAR_FREQUENCY && @stars.size < Settings::STAR_LIMIT
             @stars << Star.new(@star_anim)
         end
     end
@@ -70,32 +70,32 @@ class Player
         @beep = Gosu::Sample.new("media/beep.wav")
         @x = x 
         @y = y 
-        @vel_x = @vel_y = @angle = 0.0
+        @vel_x = @vel_y = @angle = Settings::INITIAL_VEL
         @score = 0
     end
 
     def turn_left
-        @angle -= 4.5
+        @angle -= Settings::TURN_SPEED
     end
 
     def turn_right
-        @angle += 4.5
+        @angle += Settings::TURN_SPEED
     end
 
     def accelerate
         #Offset calculates the horizontal and vertical distances that comprise a diagonal distance travelled at a given angle and pixel distance
-        @vel_x += Gosu.offset_x(@angle, 0.5)
-        @vel_y += Gosu.offset_y(@angle, 0.5)
+        @vel_x += Gosu.offset_x(@angle, Settings::ACCELERATION)
+        @vel_y += Gosu.offset_y(@angle, Settings::ACCELERATION)
     end
 
     def move
         @x += @vel_x
         @y += @vel_y
-        @x %= 640
-        @y %= 480
+        @x %= Settings::WINDOW_WIDTH
+        @y %= Settings::WINDOW_HEIGHT
 
-        @vel_x *= 0.95
-        @vel_y  *= 0.95
+        @vel_x *= Settings::DECELERATION
+        @vel_y  *= Settings::DECELERATION
     end
 
     def draw
@@ -106,8 +106,8 @@ class Player
 
     def collect_stars(stars)
         stars.reject! do | star |
-            if Gosu.distance(@x, @y, star.x, star.y) < 35
-                @score += 10
+            if Gosu.distance(@x, @y, star.x, star.y) < Settings::STAR_COLLECT_DISTANCE
+                @score += Settings::STAR_VALUE
                 @beep.play
                 true
             else
@@ -125,16 +125,20 @@ class Star
     def initialize(animation)
         @animation = animation
         @color = Gosu::Color::BLACK.dup
-        @color.red = rand(256 - 40) + 40
-        @color.green = rand(256 - 40) + 40
-        @color.blue = rand(256 - 40) + 40
-        @x = rand * 640
-        @y = rand * 480
+        @color.red = generate_color()
+        @color.green = generate_color()
+        @color.blue = generate_color()
+        @x = rand * Settings::WINDOW_WIDTH
+        @y = rand * Settings::WINDOW_HEIGHT
+    end
+
+    def generate_color
+        rand(256 - Settings::COLOR_MIN) + Settings::COLOR_MIN
     end
 
     def draw
         #Every 100 milliseconds, the next index of the animiation array is accessed
-        img = @animation[Gosu.milliseconds / 100 % @animation.size]
+        img = @animation[Gosu.milliseconds / Settings::ANIM_SPEED % @animation.size]
         img.draw(@x - (img.width / 2.0), @y - (img.height / 2.0), ZOrder::STARS, 1, 1, @color, :add)
     end
 end
--- settings.rb
+++ settings.rb
@@ -5,12 +5,37 @@ module Settings
     WINDOW_WIDTH = 640
     WINDOW_HEIGHT = 480
     CAPTION = "Star Fighter"
-    BACKGROUND = Gosu::Image.new("media/space.png", tileable: true)
 
     #################
     #Player settings#
     #################
     NEW_PLAYER_X = 320
     NEW_PLAYER_Y = 240
+    INITIAL_VEL = 0.0
+    TURN_SPEED  = 4.5
+    ACCELERATION = 0.5
+    DECELERATION = 0.95
+
+    ###############
+    #Star settings#
+    ###############
+    STARTING_STARS = []
+    STAR_FREQUENCY = 4
+    STAR_LIMIT = 25
+    STAR_COLLECT_DISTANCE = 25
+    STAR_VALUE = 10
+    COLOR_MIN = 40
+    ANIM_SPEED = 100
+
+
+
+    #############
+    #UI Settings#
+    #############
+    SCORE_FONT = Gosu::Font.new(20)
+    SCORE_COLOR = Gosu::Color::YELLOW
+    SCORE_X = 10
+    SCORE_Y = 10
+
 
 end
